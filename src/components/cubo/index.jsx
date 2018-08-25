038061721d6a862b33ac9d3a1a31ded33a23619b
Hugo Cicarelli <hcicarelli@gmail.com>
2018-08-25 16:59:30 -0300

    Começo do cubo 3d

--- src/components/cubo/index.jsx
+++ src/components/cubo/index.jsx
@@ -12,13 +12,69 @@ class CuboTesseract extends Component {
 
 
         		<div class="container">
-        			<figure class="cube">
-        				<div class="front"></div>
-        				<div class="left"></div>
-        				<div class="right"></div>
-        				<div class="top"></div>
-        				<div class="bottom"></div>
-        				<div class="back"></div>
+                    <figure class="cube first-role top left blue">
+        				<div class="side front"></div>
+        				<div class="side left"></div>
+        				<div class="side right"></div>
+        				<div class="side top"></div>
+        				<div class="side bottom"></div>
+        				<div class="side back"></div>
+        			</figure>
+                    <figure class="cube first-role top right blue">
+        				<div class="side front"></div>
+        				<div class="side left"></div>
+        				<div class="side right"></div>
+        				<div class="side top"></div>
+        				<div class="side bottom"></div>
+        				<div class="side back"></div>
+        			</figure>
+                    <figure class="cube first-role bottom left blue">
+        				<div class="side front"></div>
+        				<div class="side left"></div>
+        				<div class="side right"></div>
+        				<div class="side top"></div>
+        				<div class="side bottom"></div>
+        				<div class="side back"></div>
+        			</figure>
+                    <figure class="cube first-role bottom right blue">
+        				<div class="side front"></div>
+        				<div class="side left"></div>
+        				<div class="side right"></div>
+        				<div class="side top"></div>
+        				<div class="side bottom"></div>
+        				<div class="side back"></div>
+        			</figure>
+                    <figure class="cube back-role top left blue">
+        				<div class="side front"></div>
+        				<div class="side left"></div>
+        				<div class="side right"></div>
+        				<div class="side top"></div>
+        				<div class="side bottom"></div>
+        				<div class="side back"></div>
+        			</figure>
+                    <figure class="cube back-role top right blue">
+        				<div class="side front"></div>
+        				<div class="side left"></div>
+        				<div class="side right"></div>
+        				<div class="side top"></div>
+        				<div class="side bottom"></div>
+        				<div class="side back"></div>
+        			</figure>
+                    <figure class="cube back-role bottom left blue">
+        				<div class="side front"></div>
+        				<div class="side left"></div>
+        				<div class="side right"></div>
+        				<div class="side top"></div>
+        				<div class="side bottom"></div>
+        				<div class="side back"></div>
+        			</figure>
+                    <figure class="cube back-role bottom right blue">
+        				<div class="side front"></div>
+        				<div class="side left"></div>
+        				<div class="side right"></div>
+        				<div class="side top"></div>
+        				<div class="side bottom"></div>
+        				<div class="side back"></div>
         			</figure>
         		</div>
 
--- src/components/pages/quiz/index.jsx
+++ src/components/pages/quiz/index.jsx
@@ -16,31 +16,94 @@ class QuizPage extends Component {
                 <Header />
 
                 <div className="green-bg radial-bg">
-                    <p>
-                        Considerando uma <br/>
-                        possível <strong className="red-text">crise econômica,</strong> <br/>
-                        como que o setor da sua <br/>
-                        empresa é <strong>afetado</strong>
-                    </p>
+                    <div id="question-1" className="question-wrapper hidden">
+                        <p>
+                            Considerando uma <br/>
+                            possível <strong className="red-text">crise econômica,</strong> <br/>
+                            como que o setor da sua <br/>
+                            empresa é <strong>afetado</strong>
+                        </p>
 
-                    <p align="center">selecione:</p>
+                        <p align="center">selecione:</p>
 
-                    <div className="question-box-container">
-                        <div className="question-box">
-                            <div className="red-bg question-header">
-                                <h3>Maior <span>exposição</span></h3>
+                        <div className="question-box-container">
+                            <div className="question-box">
+                                <div className="red-bg question-header">
+                                    <h3>Maior <span>exposição</span></h3>
+                                </div>
+                                <div className="info-box">
+                                    <p className="red-text">Empresas de turismo ou veículos novos que podem ser afetados significativamente pela crise</p>
+                                </div>
                             </div>
-                            <div className="info-box">
-                                <p className="red-text">Empresas de turismo ou veículos novos que podem ser afetados significativamente pela crise</p>
+
+                            <div className="question-box">
+                                <div className="blue-bg question-header">
+                                    <h3>Menor <span>exposição</span></h3>
+                                </div>
+                                <div className="info-box">
+                                    <p className="blue-text">Empresas de setor de alimentação básica, assim como de medicamentos são menos afetadas pela crise</p>
+                                </div>
                             </div>
                         </div>
+                    </div>
 
-                        <div className="question-box">
-                            <div className="blue-bg question-header">
-                                <h3>Menor <span>exposição</span></h3>
+                    <div id="question-2" className="question-wrapper hidden">
+                        <p>
+                            Considerando o posicionamento <br/>
+                            <strong className="blue-text">estratégico</strong> da sua empresa <br/>
+                            como você acredita que ela é <br/>
+                            <strong>Forte ou Fraca?</strong>
+                        </p>
+
+                        <p align="center">selecione:</p>
+
+                        <div className="question-box-container">
+                            <div className="question-box">
+                                <div className="red-bg question-header">
+                                    <h3>Forte</h3>
+                                </div>
+                                <div className="info-box">
+                                    <p className="red-text">Empresas de turismo ou veículos novos que podem ser afetados significativamente pela crise</p>
+                                </div>
                             </div>
-                            <div className="info-box">
-                                <p className="blue-text">Empresas de setor de alimentação básica, assim como de medicamentos são menos afetadas pela crise</p>
+
+                            <div className="question-box">
+                                <div className="blue-bg question-header">
+                                    <h3>Fraca</h3>
+                                </div>
+                                <div className="info-box">
+                                    <p className="blue-text">Empresas de setor de alimentação básica, assim como de medicamentos são menos afetadas pela crise</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id="question-3" className="question-wrapper">
+                        <p>
+                            Você considera que a <br/>
+                            <strong className="blue-text">Posição Financeira</strong> da sua <br/>
+                            empresa é <strong>Forte ou Fraca?</strong>
+                        </p>
+
+                        <p align="center">selecione:</p>
+
+                        <div className="question-box-container">
+                            <div className="question-box">
+                                <div className="red-bg question-header">
+                                    <h3>Forte</h3>
+                                </div>
+                                <div className="info-box">
+                                    <p className="red-text">Capital de giro é financiado exclusivamente com capital próprio e gerado pela operação principal do negócio e o fluxo de caixa é positivo honrando seus compromissos em dia através da geração de Lucro e Valor.</p>
+                                </div>
+                            </div>
+
+                            <div className="question-box">
+                                <div className="blue-bg question-header">
+                                    <h3>Fraca</h3>
+                                </div>
+                                <div className="info-box">
+                                    <p className="blue-text">Sua empresa depende de financiamentos periódicos tanto de terceiros como bancos ou fornecedores ou dos recursos dos sócios vindos de outras fontes.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
