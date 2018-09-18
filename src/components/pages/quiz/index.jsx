aa9af047f8f284693ffb561ecd19d9165a9f053c
Hugo Cicarelli <hcicarelli@gmail.com>
2018-09-18 07:14:20 -0300

    Popula textos

--- src/components/pages/quiz/index.jsx
+++ src/components/pages/quiz/index.jsx
@@ -16,11 +16,11 @@ class QuizPage extends Component {
             perguntas: [
                 {
                     "id": 1,
-                    "texto_pergunta": "Considerando o Setor de atuação da sua empresa, qual o grau de exposição dela frente a crises?",
+                    "texto_pergunta": "Considerando uma possível crise econômica, como que o setor da sua empresa é afetado?",
                     "texto_resposta_1": "Maior",
                     "texto_resposta_2": "Menor",
-                    "descritivo_resposta_1": "A definir",
-                    "descritivo_resposta_2": "A definir",
+                    "descritivo_resposta_1": "Empresas de turismo ou veículos novos que podem ser afetados significativamente pela crise",
+                    "descritivo_resposta_2": "Empresas do setor de alimentação básica, assim como de medicamentos são menos afetadas pela crise",
                     "created_at": "2018-09-17 19:12:27",
                     "updated_at": "2018-09-17 19:12:27",
                     "deleted_at": null
